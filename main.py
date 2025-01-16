b2bcbffc7d7bcd543b2676d5dbbe9c946147263b
Tyrone van Balla <tyronevb@live.com>
2025-01-16 08:03:20 +0200

    add function to sort character occurrences in descending order

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ def main():
     book_text = get_book_text(book_path)
     num_words = count_words(book_text)
     char_count = count_character_occurence(book_text)
-    
-    print_report(book_path, num_words, char_count)
+    sorted_char_count = sort_char_counts(char_count)
+    print_report(book_path, num_words, sorted_char_count)
 
 def get_book_text(path_to_book):
     with open(path_to_book) as f:
@@ -26,12 +26,24 @@ def count_character_occurence(book_text):
     
     return character_count
 
-def print_report(book, num_words, char_occurence):
-    print(f"--- Begin report for {book} ---")
+def print_report(book, num_words, sorted_char_occurences):
+    print(f"\n--- Begin report for {book} ---")
     print(f"The book '{book}' contains {num_words} words.\n")
 
-    for char in char_occurence:
-        if char.isalpha():
-            print(f"The '{char}' character occurs {char_occurence[char]} times.")
+    for char in sorted_char_occurences:
+        if char["character"].isalpha():
+            print(f"The '{char['character']}' character occurs {char['occurrence']} times.")
+
+    print("\n--- End Report ---")
+
+def sort_char_counts(char_occurences):
+    list_of_dicts = []
+    for char in char_occurences:
+        list_of_dicts.append({'character':char, "occurrence":char_occurences[char]})
+    list_of_dicts.sort(reverse=True, key=sort_on)
+    return list_of_dicts
+
+def sort_on(dict):
+    return dict["occurrence"]
 
 main()
