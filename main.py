cd8d8db3cd8d0d70d762204e2867e4d2ba199851
Tyrone van Balla <tyronevb@live.com>
2025-01-16 07:46:08 +0200

    add function to count the occurence of each character in a book

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ def main():
     book_text = get_book_text(book_path)
     num_words = count_words(book_text)
     print(f"The book: {book_path} contains {num_words} words.")
+    char_count = count_character_occurence(book_text)
+    print(char_count)
 
 def get_book_text(path_to_book):
     with open(path_to_book) as f:
@@ -13,5 +15,15 @@ def get_book_text(path_to_book):
     
 def count_words(book_text):
     return len(book_text.split())
+
+def count_character_occurence(book_text):
+    character_count = {}
+    for char in book_text.lower():
+        if char in character_count:
+            character_count[char] += 1
+        else:
+            character_count[char] = 1
+    
+    return character_count
 
 main()
